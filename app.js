0e0e86bb2215ea54c731d9216db93869f64bf4ed
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-15 21:50:29 -0700

    passed post route variables to render from app.js inside home.ejs

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("home");
+    res.render("home", {startText :homeStartingContent});
 });
 
 
